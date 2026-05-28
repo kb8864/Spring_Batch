d0abd4d281733a597aa63f511c9d0113e24a28f8
kb8864 <yt7s858864@gmail.com>
2026-05-28 12:32:53 +0900

    指定したTaskletをStepに注入し、Step実行時に動かすためアノテーションを追加

--- hello-spring-batch/src/main/java/com/example/demo/tasklet/HelloTasklet1.java
+++ hello-spring-batch/src/main/java/com/example/demo/tasklet/HelloTasklet1.java
@@ -1,11 +1,14 @@
 package com.example.demo.tasklet;
 
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.StepContribution;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
-
+@Component("HelloTasklet1")
+@StepScope
 public class HelloTasklet1 implements Tasklet{
 
 	@Override
